93b0a7a37ad45ca759eba6a07d5f6ea48cbe6f2c
Nabeel <nabeel.a@origen.co>
2025-06-29 18:35:37 +0500

    fix: update app.run to use environment variable for port configuration

--- app.py
+++ app.py
@@ -361,4 +361,7 @@ def export_csv():
                     headers={"Content-Disposition": "attachment;filename=shortlisted_candidates.csv"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT not set
+    app.run(host="0.0.0.0", port=port)
+ 
+ 
